8defacd29d08856dfaef67aedd37617b817bbca0
Scott Schaefer <sschaefer@rms.com>
2019-11-23 19:40:48 -0500

    update and fix css errors:

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
